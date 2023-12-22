e123a2b957de8a0519283fdb82d54b14234ffc61
전민주 <junielle.jeon@gmail.com>
2023-12-22 19:23:42 +0900

    feat: tailwindcss, nav style add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
 	return (
-		<header>
-			<Link to='/'>
+		<header className="flex justify-between border-b border-gray-300 p-2">
+			<Link to='/' className="flex items-center text-4xl text-brand">
 				{/* 아이콘 추가 필요 */}
 				<h1>Minju Shop</h1>
 			</Link>
 
-			<nav>
+			<nav className="flex items-center gap-4">
 				<Link to='/products'>Products</Link>
 				<Link to='/carts'>Carts</Link>
-				<Link to='/products/new'>Products</Link>
+				<Link to='/products/new' className="text-2xl">Products</Link>
 
 				{/* login */}
 				<button type='button'>Login</button>
