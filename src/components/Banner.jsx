ab68ccfd4b043ef60553e5504986cbd0876f26e2
전민주 <junielle.jeon@gmail.com>
2024-02-14 21:18:21 +0900

    feat: banner text edit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ export default function Banner() {
 			<div className='w-full h-full bg-cover bg-banner opacity-80 absolute top-0'></div>
 			<div className='absolute w-full top-32 text-center text-gray-50 drop-shadow-2xl'>
 				<h2 className='text-6xl'>Let's go Backpacking</h2>
-				<span className='text-2xl'>!!!!!!~~~~</span>
+				<p className='text-2xl pt-2'>We invite you to the world of backpacking.</p>
 			</div>
 		</section>
 	)
